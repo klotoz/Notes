5976dc8f2a89e687d78c4b3ca51fa9423efe1b5f
klotoz <ikskulfg@gmail.com>
2021-02-19 14:14:09 +0300

    фрагмент и лэйаут для открытия заметки

--- app/src/main/java/com/example/notes/SecondActivity.java
+++ app/src/main/java/com/example/notes/SecondActivity.java
@@ -6,5 +6,23 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null) {
+            NoteFragment details = new NoteFragment();
+            details.setArguments(getIntent().getExtras());
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, details).commit();
+        }
+    }
 
 }
