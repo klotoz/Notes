92d09f8bc71ef12d31ab654cc4187e61d0b45ab3
klotoz <ikskulfg@gmail.com>
2021-02-19 14:13:18 +0300

    фрагмент и активити для листа со всеми заметками

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -10,5 +10,60 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity implements Observer {
 
+    private boolean isLandscape;
+    private NoteFragment noteFragment;
+    private ArrayList<Note> noteList = new ArrayList<>();
+    private static final String NOTE_LIST = "notes";
+    private static final String NOTE = "note";
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initNoteList();
+        NotesListFragment listOfNotesFragment = NotesListFragment.newInstance();
+        Bundle bundle = new Bundle();
+        bundle.putParcelableArrayList(NOTE_LIST, noteList);
+        listOfNotesFragment.setArguments(bundle);
+        noteFragment = new NoteFragment();
+
+        isLandscape = getResources().getConfiguration().orientation
+                == Configuration.ORIENTATION_LANDSCAPE;
+        if (isLandscape) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.listFragmentContainer, listOfNotesFragment)
+                    .replace(R.id.noteFragmentContainer, noteFragment)
+                    .commit();
+            listOfNotesFragment.subscribe(noteFragment);
+        } else {
+            setFragToContainer(listOfNotesFragment);
+            listOfNotesFragment.subscribe(this);
+        }
+    }
+
+    @Override
+    public void openNote(Note note) {
+        Bundle bundle = new Bundle();
+        bundle.putParcelable(NOTE, note);
+        noteFragment.setArguments(bundle);
+        setFragToContainer(noteFragment);
+    }
+
+    private void setFragToContainer(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.fragment_container, fragment)
+                .addToBackStack(null)
+                .commit();
+    }
+
+    private void initNoteList() {
+        noteList.add(new Note("Название_1", "Описание_1"));
+        noteList.add(new Note("Название_2", "Описание_2 Вот тут будет некрасиво дофига всего потому что я не понимаю, как это все будет выглядеть на экране,  и вообще, приятно хоть что-то написать на русском языке, пусть и с ошибками и опечатками) О! Кажется, моя графомания дает о себе знать))) Лучше бы так с кодом было!"));
+        noteList.add(new Note("Название_3", "Описание_3"));
+        noteList.add(new Note("Название_4", "Описание_4"));
+        noteList.add(new Note("Название_5", "Описание_5"));
+
+    }
 }
--- app/src/main/java/com/example/notes/NotesListFragment.java
+++ app/src/main/java/com/example/notes/NotesListFragment.java
@@ -18,5 +18,70 @@ import java.util.List;
 
 public class NotesListFragment extends Fragment implements NoLiNot {
 
+    private final List<Observer> noteListObservers;
+    private ArrayList<Note> noteList;
+    private static final String NOTE_LIST = "notes";
 
+    public NotesListFragment() {
+        noteListObservers = new ArrayList<>();
+        noteList = new ArrayList<>();
+    }
+
+    public static NotesListFragment newInstance() {
+        return new NotesListFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Bundle bundle = getArguments();
+        if (bundle != null) {
+            noteList = bundle.getParcelableArrayList(NOTE_LIST);
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_notes_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initList(view);
+    }
+
+    private void initList(View view) {
+        LinearLayout layout = (LinearLayout) view;
+        for (Note note : noteList) {
+            TextView textView = new TextView(getContext());
+            textView.setText(note.getTitle());
+            textView.setTextSize(getResources().getDimension(R.dimen.t_title_size));
+            textView.setBackground(getResources().getDrawable(R.drawable.et_style));
+            float paddingH = getResources().getDimension(R.dimen.padding_h);
+            float paddingV = getResources().getDimension(R.dimen.padding_v);
+            textView.setPaddingRelative((int) paddingH, (int) paddingV, (int) paddingH, (int) paddingV);
+            layout.addView(textView);
+
+            textView.setOnClickListener(view1 -> noteClicked(note));
+        }
+    }
+
+    @Override
+    public void subscribe(Observer observer) {
+        noteListObservers.add(observer);
+    }
+
+    @Override
+    public void unsubscribe(Observer observer) {
+        noteListObservers.remove(observer);
+    }
+
+    @Override
+    public void noteClicked(Note note) {
+        for (Observer noteListObserver : noteListObservers) {
+            noteListObserver.openNote(note);
+        }
+    }
 }
