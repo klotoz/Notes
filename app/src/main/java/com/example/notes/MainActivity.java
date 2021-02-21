d807ebe8ef3e81a1185f287af738c66585bfb62f
klotoz <ikskulfg@gmail.com>
2021-02-21 14:32:33 +0300

    homework7

--- app/src/main/java/com/example/notes/MainActivity.java
+++ app/src/main/java/com/example/notes/MainActivity.java
@@ -1,10 +1,22 @@
 package com.example.notes;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 
 import android.content.res.Configuration;
+
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
 
 import java.util.ArrayList;
 
@@ -20,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements Observer {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = initToolbar();
+        initDrawer(toolbar);
         initNoteList();
         NotesListFragment listOfNotesFragment = NotesListFragment.newInstance();
         Bundle bundle = new Bundle();
@@ -37,7 +51,7 @@ public class MainActivity extends AppCompatActivity implements Observer {
                     .commit();
             listOfNotesFragment.subscribe(noteFragment);
         } else {
-            setFragToContainer(listOfNotesFragment);
+            addFragment(listOfNotesFragment);
             listOfNotesFragment.subscribe(this);
         }
     }
@@ -47,10 +61,10 @@ public class MainActivity extends AppCompatActivity implements Observer {
         Bundle bundle = new Bundle();
         bundle.putParcelable(NOTE, note);
         noteFragment.setArguments(bundle);
-        setFragToContainer(noteFragment);
+        addFragment(noteFragment);
     }
 
-    private void setFragToContainer(Fragment fragment) {
+    private void addFragment(Fragment fragment) {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fragment_container, fragment)
@@ -65,5 +79,69 @@ public class MainActivity extends AppCompatActivity implements Observer {
         noteList.add(new Note("Название_4", "Описание_4"));
         noteList.add(new Note("Название_5", "Описание_5"));
 
+    }
+
+    private void initDrawer(Toolbar toolbar) {
+        final DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int id = item.getItemId();
+                if (navigateFragment(id)) {
+                    drawer.closeDrawer(GravityCompat.START);
+                    return true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private Toolbar initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        return toolbar;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (navigateFragment(id)) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private boolean navigateFragment(int id) {
+        switch (id) {
+            case R.id.action_settings:
+                Toast.makeText(MainActivity.this, "настройки", Toast.LENGTH_SHORT).show();
+                addFragment(new FragmentAbout());
+                return true;
+            case R.id.action_main:
+                Toast.makeText(MainActivity.this, "main", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.action_favorite:
+                Toast.makeText(MainActivity.this, "favorite", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/notes/NotesListFragment.java
+++ app/src/main/java/com/example/notes/NotesListFragment.java
@@ -5,7 +5,10 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -84,4 +87,6 @@ public class NotesListFragment extends Fragment implements NoLiNot {
             noteListObserver.openNote(note);
         }
     }
+
+
 }
